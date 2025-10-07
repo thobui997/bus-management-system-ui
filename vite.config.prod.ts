269b5ccc09989c8bb739288b1851b1995968cf34
tho.bui <thobui97@outlook.com>
2025-10-07 12:16:16 +0700

    fix: add config tailwincss for prd

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000
   },
