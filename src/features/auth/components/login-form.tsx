800ce9bccd2d800934713f018b546fe3ac1fb028
thobui997 <thobui97@outlook.com>
2025-10-09 15:53:01 +0700

    style : recreate ui for login page

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,4 +1,4 @@
-import { UserOutlined } from '@ant-design/icons';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
 import { useAuth } from '@app/context/auth-context';
 import { Button, Form, Input } from 'antd';
 import './login-form.scss';
@@ -15,29 +15,63 @@ const LoginForm = () => {
 
   return (
     <div id='Login'>
-      <div className='login-container'>
-        <h2>Login</h2>
+      {/* Left Side - Image & Branding */}
+      <div className='login-left'>
+        <div className='login-image-container'>
+          <img src='https://illustrations.popsy.co/amber/remote-work.svg' alt='Bus Management System' />
+          <div className='login-title'>
+            <h1>BusManager System</h1>
+            <p>Manage your fleet with ease and efficiency</p>
+          </div>
+        </div>
+      </div>
 
-        <Form form={form} name='loginForm' layout='vertical' autoComplete='off'>
-          <Form.Item label='Username' name='username' rules={[{ required: true, message: 'Username is required!' }]}>
-            <Input
-              prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-              size='large'
-              placeholder='Enter username'
-              allowClear
-            />
-          </Form.Item>
+      {/* Right Side - Login Form */}
+      <div className='login-right'>
+        <div className='login-container'>
+          <div className='login-header'>
+            <div className='logo'>
+              <div className='logo-image'>
+                <img src='/images/bus.png' alt='logo' />
+              </div>
+              <div className='logo-text'>BusManager</div>
+            </div>
+            <h2>Welcome Back</h2>
+            <p>Please sign in to your account</p>
+          </div>
 
-          <Form.Item label='Password' name='password' rules={[{ required: true, message: 'Password is required!' }]}>
-            <Input.Password size='large' placeholder='Enter password' allowClear />
-          </Form.Item>
+          <Form form={form} name='loginForm' layout='vertical' autoComplete='off' onFinish={handleLogin}>
+            <Form.Item label='Email' name='username' rules={[{ required: true, message: 'Email is required!' }]}>
+              <Input
+                prefix={<MailOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+                size='large'
+                placeholder='Enter your email'
+                allowClear
+              />
+            </Form.Item>
 
-          <Form.Item label={null}>
-            <Button type='primary' size='large' htmlType='button' style={{ width: '100%' }} onClick={handleLogin}>
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item label='Password' name='password' rules={[{ required: true, message: 'Password is required!' }]}>
+              <Input.Password
+                prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+                size='large'
+                placeholder='Enter your password'
+                allowClear
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type='primary' size='large' htmlType='submit' style={{ width: '100%', marginTop: 16 }}>
+                Sign In
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <div className='login-footer'>
+            <p>
+              Don't have an account? <a href='#'>Contact Administrator</a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
